44cc99f00127550d46e375f29aff7b9c9247b168
M Isa Al Anshori <38964181+misaalanshori@users.noreply.github.com>
2020-07-13 14:55:35 +0700

    did a bit of things
    i still dont know what to put here

--- internetClock.py
+++ internetClock.py
@@ -1,46 +1,47 @@
+#prints the current time sourced from a web api, time gets updated instantly and the time is printed every 0.25 seconds with different thread
 import threading
 import requests
 import json
 import time
 import sys
 timedata = ""
-info = ""
+info = "Loading..."
 die = False
+seconds = 0.0
+
 def getTime():
-    global timedata, info
+    global timedata, info, seconds
     while True:
         if die:
             break
         try:
             req = requests.get("http://worldtimeapi.org/api/timezone/Asia/Jakarta")
             info = "Last Update Succeeded"
+            seconds = 0.0
         except:
             info = "Last Update Failed"
             return
         timeJson = json.loads(req.text)
         timeStr = timeJson["datetime"] + " " + timeJson["abbreviation"]
         timedata = timeStr
-        time.sleep(1)
 
 def printTime():
+    global seconds
     while True:
         if die:
             break
-        print("The current time is: " + timedata + "({})".format(info))
+        seconds += 0.25
+        sys.stdout.write("\rThe current time is: " + timedata + "({}, {} Seconds ago)".format(info, seconds) + "   ")
+        sys.stdout.flush()
         time.sleep(0.25)
 
-# timeSync = threading.Thread(target=getTime)
-# timePrint = threading.Thread(target=printTime)
-# timeSync.start()
-# timePrint.start()
-# timeSync.join()
 
 try:
     timeSync = threading.Thread(target=getTime)
     timePrint = threading.Thread(target=printTime)
     timeSync.start()
     timePrint.start()
-    while timePrint.isAlive(): 
+    while timePrint.is_alive(): 
         timePrint.join(1)  # not sure if there is an appreciable cost to this.
 except (KeyboardInterrupt, SystemExit):
     die = True
